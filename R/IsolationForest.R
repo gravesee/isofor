f868e541bf39c07de04f26df66bcedf485e0d961
Zelazny7 <gravcon5@gmail.com>
2016-10-12 18:03:31 -0400

    added some safeguards for predict

--- R/IsolationForest.R
+++ R/IsolationForest.R
@@ -56,9 +56,31 @@ iTree <- function(X, l) {
   mat
 }
 
+#' @title iForest
+#'
+#' @description Build an Isolation Forest of completly random trees
+#'
+#' @param X a matrix or data.frame of numeric or factors values
+#' @param nt the number of trees in the ensemble
+#' @param phi the number of samples to draw without replacement to construct each tree
+#'
+#' @details An Isolation Forest is an unsupervised anomaly detection algorithm. The requested
+#' number of trees, \code{nt}, are built completely at random on a subsample of size \code{phi}.
+#' At each node a random variable is selected. A random split is chosen from the range of that
+#' variable. A random sample of factor levels are chosen in the case the variable is a factor.
+#'
+#' Records from \code{X} are then filtered based on the split criterion and the tree building
+#' begins again on the left and right subsets of the data. Tree building terminates when the
+#' maximum depth of the tree is reached or there are 1 or fewer observations in the filtered
+#' subset.
+#'
+#' @return an \code{iForest} object
+#'
 #' @export
-iForest <- function(X, nt, phi) {
+iForest <- function(X, nt=100, phi=256) {
   l = ceiling(log(phi, 2))
+
+  if (!is.data.frame(X)) X <- as.data.frame(X)
 
   forest = vector("list", nt)
   for (i in 1:nt) {
@@ -86,13 +108,13 @@ pl_factor <- function(x, i, v, Forest) {
   x[,i,TRUE] %in% l[f]
 }
 
-iTreeFilter <- function(x, ni, Tree, Forest) UseMethod("iTreeFilter")
+iTreeFilter <- function(x, ni, Tree, Forest, i) UseMethod("iTreeFilter")
 
-iTreeFilter.numeric <- function(x, ni, Tree, Forest) {
+iTreeFilter.numeric <- function(x, ni, Tree, Forest, i) {
   x < Tree[ni,"SplitValue"]
 }
 
-iTreeFilter.factor <- function(x, ni, Tree, Forest) {
+iTreeFilter.factor <- function(x, ni, Tree, Forest, i) {
   i = Tree[ni,"SplitAtt"]
   v = Tree[ni,"SplitValue"]
   l = Forest$vLevels[[i]]
@@ -101,17 +123,37 @@ iTreeFilter.factor <- function(x, ni, Tree, Forest) {
 }
 
 pathLength <- function(x, Tree, Forest, e=0, ni=1) {
+  if (!is.data.frame(x)) x <- as.data.frame(x)
   if (Tree[ni,"Type"] == -1) return(e + Tree[ni,"Size"])
   i  = Tree[ni,"SplitAtt"]
 
   ifelse(
-    iTreeFilter(x[,i,TRUE], ni, Tree, Forest),
+    iTreeFilter(x[,i,TRUE], ni, Tree, Forest, i),
     pathLength(x, Tree, Forest, e + 1, Tree[ni,"Left"]),
     pathLength(x, Tree, Forest, e + 1, Tree[ni,"Right"]))
 }
 
+
+#' @title predcit.iForest
+#'
+#' @description predict.iForest is a method of the predict generic function.
+#'
+#' @param object an \code{iForest} object
+#' @param newdata a dataset to predict
+#'
+#'
 #' @export
 predict.iForest <- function(object, newdata, ...) {
+  if (!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
+
+  i = match(object$vNames, names(newdata))
+
+  if (any(is.na(i))) {
+    m = object$vNames[!object$vNames %in% names(newdata)]
+    stop(strwrap(c("Variables found in model not found in newdata: ",
+         paste0(m, collapse = ", ")), width = 80, prefix = " "), call. = F)
+  }
+
   pls = sapply(object$forest, function(f) pathLength(newdata, f, object))
   2^(-rowMeans(pls)/cn(object$phi))
 }
@@ -122,18 +164,24 @@ print.iForest <- function(x, ...) {
 }
 
 
-## optimize the prediction routine
-N = 1e3
-x = c(rnorm(N, 0, 0.5), rnorm(N*0.05, -1.5, 1))
-y = c(rnorm(N, 0, 0.5), rnorm(N*0.05,  1.5, 1))
-data = data.frame(x, y)
-
-mod = iForest(X = data, 100, 32)
-p = predict(mod, data)
-col = ifelse(p > quantile(p, 0.95), "red", "blue")
-plot(x, y, col=col)
-
-km = kmeans(data, 2)
-plot(x, y, col=km$cluster+1)
-
-
+# ## optimize the prediction routine
+# N = 1e3
+# x = c(rnorm(N, 0, 0.5), rnorm(N*0.05, -1.5, 1))
+# y = c(rnorm(N, 0, 0.5), rnorm(N*0.05,  1.5, 1))
+# data = data.frame(x, y)
+#
+# mod = iForest(X = data, 100, 32)
+# p = predict(mod, data)
+# col = ifelse(p > quantile(p, 0.95), "red", "blue")
+# plot(x, y, col=col)
+#
+# km = kmeans(data, 2)
+# plot(x, y, col=km$cluster+1)
+#
+#
+# data(titanic, package="binnr2")
+# X = titanic
+# X$Age[is.na(X$Age)] <- median(X$Age, TRUE)
+#
+# mod = iForest(X, phi=32)
+# p = predict(mod, X[-(1:3)])

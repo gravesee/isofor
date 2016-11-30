eccc6cd7cb2bcf81f12c8b8c3e880bb028220390
Eric Graves <eric.graves@lexisnexis.com>
2016-11-30 17:50:58 -0600

    working on cpp induction algorithm

--- src/induce.cpp
+++ src/induce.cpp
@@ -1,69 +1,301 @@
+#include <Rmath.h>
 #include <Rcpp.h>
+#include <R.h>
 #include <Rinternals.h>
 #include <stdlib.h>
 #include <bitset>
 #include <math.h>
-using namespace Rcpp;
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <string.h>
+#include <bitset>
+
+//using namespace Rcpp;
 using namespace std;
 
-//enum Tree { Type, Size, Left, Right, SplitAtt, SplitValue, AttType };
-//enum vType { Numeric = 1, Factor = 2};
+enum Tree { Type, Size, Left, Right, SplitAtt, SplitValue, AttType };
+enum vType { R_NUMERIC = 1, R_FACTOR = 2, R_INTEGER = 3};
 //enum ForestSlots { FOREST=0, PHI=1, NTREES=3};
+
+// class to store column and column info/operations range, etc...
+class Column {
+public:
+  SEXP data;
+  int size;
+  int type;
+  Column(SEXP data);
+  double random_split(vector<int> in_bag);
+  bool compare(int index, double value);
+  bool compare(int index, std::set<int> value);
+};
+
+Column::Column(SEXP data) {
+  this->data = data;
+  size = LENGTH(data);
+  if (Rf_isFactor(data)) {
+    type = R_FACTOR;
+  } else if (Rf_isReal(data)) {
+    type = R_NUMERIC;
+  } else if (Rf_isInteger(data)) {
+    type = R_INTEGER;
+  }
+}
+
+bool Column::compare(int index, double value) {
+  if (type == R_NUMERIC) {
+    return(REAL(data)[index] < value);
+  } else {
+    return(INTEGER(data)[index] < value);
+  }
+}
+
+bool Column::compare(int index, std::set<int> value) {
+  return(value.count(INTEGER(data)[index]));
+}
+
+class DataFrame {
+  std::vector<Column> Columns;
+
+public:
+  std::vector<int> indices;
+  int num_rows, num_cols;
+
+  DataFrame() {indices = vector<int>(0); num_rows = 0; num_cols=0;}
+  DataFrame(SEXP data);
+  void print_var_types();
+  void fill_indices();
+  Column get_column(int i);
+
+
+};
+
+Column DataFrame::get_column(int i) {
+  return Columns[i];
+}
+
+double random_split_numeric(Column c, vector<int> in_bag) {
+  double min = R_PosInf;
+  double max = R_NegInf;
+  double * d = REAL(c.data);
+  for(std::vector<int>::iterator it = in_bag.begin(); it != in_bag.end(); ++it) {
+    if (d[*it] < min) min = d[*it];
+    if (d[*it] > max) max = d[*it];
+  }
+  return unif_rand() * (max - min) + min;
+}
+
+double random_split_integer(Column c, vector<int> in_bag) {
+  double min = R_PosInf;
+  double max = R_NegInf;
+  int * d = INTEGER(c.data);
+  for(std::vector<int>::iterator it = in_bag.begin(); it != in_bag.end(); ++it) {
+    if (d[*it] < min) min = d[*it];
+    if (d[*it] > max) max = d[*it];
+  }
+  return unif_rand() * (max - min) + min;
+}
+
+double random_split_factor(Column c, vector<int> in_bag) {
+  int * d = INTEGER(c.data);
+
+  std::set<int> uniq;
+
+  // load the vector
+  for(std::vector<int>::iterator it = in_bag.begin(); it != in_bag.end(); ++it) {
+    uniq.insert(d[*it]);
+  }
+
+  // randomly sample from range of unique values
+  int s = unif_rand() * pow(uniq.size(), 2);
+
+  std::bitset<32> sampled_levels(s); // to ones and zeros depending on sampled stats
+  std::bitset<32> out(0); // initialized to zero
+
+  // copy set to vector
+  std::vector<int> res(uniq.begin(), uniq.end());
+
+  for(int i = 0; i < sampled_levels.size(); i++) {
+    if (sampled_levels[i] == 1) {
+      out[res[i] - 1] = 1; // R factors are 1-indexed
+    }
+  }
+
+  // convert back to double
+  return(out.to_ullong());
+}
+
+// function to take a double value and return the set
+std::set<int> create_set_from_double(double val) {
+  std::set<int> out;
+  std::bitset<32> bitset(val);
+
+  for (int i = 0; i < bitset.size(); i++) {
+    if (bitset[i] == 1) {
+      out.insert(i + 1); // add one because R Factors are 1-indexed
+    }
+  }
+  return(out);
+}
+
+
+double Column::random_split(vector<int> in_bag) {
+  if (type == R_NUMERIC) {
+    return(random_split_numeric(*this, in_bag));
+  } else if (type == R_INTEGER) {
+    return(random_split_integer(*this, in_bag));
+  } else if (type == R_FACTOR) {
+    return(random_split_factor(*this, in_bag));
+  } else {
+    return(-1.0);
+  }
+}
+
+DataFrame::DataFrame(SEXP data) {
+  num_cols = LENGTH(data);
+  num_rows = LENGTH(VECTOR_ELT(data, 0)); // length of first data.frame column
+  indices  = vector<int>(num_rows);
+  std::iota(indices.begin(), indices.end(), 0);
+
+  for (int i = 0; i < num_cols; i++) {
+    Columns.push_back(Column(VECTOR_ELT(data, i)));
+  }
+}
+
+void DataFrame::print_var_types() {
+  for(std::vector<Column>::iterator it = Columns.begin(); it != Columns.end(); ++it) {
+    Rprintf("%d ", (*it).type);
+  }
+  Rprintf("\n");
+}
+
 
 class iForest {
 
-  std::vector<double**> forest;
+  //std::vector<double**> forest;
+
+  DataFrame df;
+  // pointer to the data
+  //bool * mask; // reused boolean mask for growing the tree
 
   public:
-    double l; // max depth
-    int nt; // number of trees
-    int phi; // number of sample used to calculate max depth
+    vector<int> in_bag;
+    int max_depth; // max depth
+    int num_trees; // number of trees
+    int num_samples; // number of sample used to calculate max depth
     int max_nodes;
-
-    // also need pointer to list of data
-
-    // also need the variable types (factor and numeric only)
+    bool * mask;
 
     // constructor
-    iForest(int num_trees, int num_samples);
+    iForest(int num_trees, int num_samples, SEXP data);
+
+    // Destructor
+    ~iForest();
+
+    int num_cols() { return df.num_cols; }
+    int num_rows() { return df.num_rows; }
+    Column get_column(int i)  { return df.get_column(i);}
 
     // grow multiple trees in succession
     void grow_forest(void);
 
-    // grow a single tree
-    double * grow_tree(void);
+    void recurse(int e, int node_id, double* m);
+
+    double * grow_tree(void); // grow a single tree
+
+    void clear_boolean_mask();
+
+    void subsample();
 
 };
 
 
-// implement the class
-// Member functions definitions including constructor
-iForest::iForest(int num_trees, int num_samples) {
-  nt = num_trees;
-  phi = num_samples;
-  l = ceil(log2(phi));
-  max_nodes = (pow(l, 2) - 1) * pow(l, 2);
+// constructor
+
+iForest::iForest(int num_trees, int num_samples, SEXP data) {
+  df = DataFrame(data);
+  this->num_trees = num_trees;
+  this->num_samples = num_samples > df.num_rows ? df.num_rows : num_samples;
+  in_bag = vector<int>(num_samples);
 }
 
+iForest::~iForest() {
+  free(mask);
+}
 
+void iForest::clear_boolean_mask() {
+  mask = (bool*) calloc(num_samples * max_nodes, sizeof(bool));
 
-double * iForest::grow_tree() {
-  // allocate memory for the output matrix
-
-  double * m = (double *) calloc(max_nodes * 7, sizeof(double));
-
-  return(m);
+  // set first column to all true
+  for (int i = 0; i < num_samples; i++) {
+    mask[i] = true;
+  }
 
 }
 
-  //return(m);
-//}
+// function that clears the in_bag vector and then randomly fills it with
+// indices totally the number of requested samples
 
-//}
-//env$mat = matrix(0,
-//                 nrow = max_nodes(l),
-//                 ncol = 7,
-//                 dimnames = list(NULL,
-//                                 c("Type","Size","Left","Right","SplitAtt","SplitValue","AttType")))
+void iForest::subsample() {
+  // randomly shuffle the indices and fill in_bag
+  std::random_shuffle(df.indices.begin(), df.indices.end());
+
+  for (int i = 0; i < num_samples; i++) {
+    in_bag[i] = df.indices[i];
+  }
+}
+
+// create boolean masks, intial one is all true, rest are false
 
 
+
+// THis is the function that passes stuff from R to C and back
+RcppExport SEXP test_func (SEXP data) {
+
+  iForest mod = iForest(100, 16, data);
+
+  // create new mask
+  mod.clear_boolean_mask();
+
+  // randomly sample data points
+  mod.subsample();
+
+
+  // get a random column
+  int i = unif_rand() * mod.num_cols();
+  Column col = mod.get_column(i);
+
+  // get a random split
+  double split = col.random_split(mod.in_bag);
+
+  // create the boolean mask using the column and the split value
+  int l_node_id = 1;
+  int r_node_id = 2;
+
+  if (col.type == R_FACTOR) {
+    std::set<int> comp = create_set_from_double(split);
+
+    for (int i = 0; i < mod.num_samples; i++) {
+      bool test = col.compare(i, comp);
+      mod.mask[mod.num_samples * l_node_id + i] &= test;
+      mod.mask[mod.num_samples * r_node_id + i] &= !test;
+    }
+  } else {
+    for (int i = 0; i < mod.num_samples; i++) {
+      bool test = col.compare(i, split);
+      mod.mask[mod.num_samples * l_node_id + i] &= test;
+      mod.mask[mod.num_samples * r_node_id + i] &= !test;
+    }
+  }
+
+  Rprintf("Random Split Value: %f\n", split);
+
+  if (col.type == R_FACTOR) {
+    std::set<int> tmp = create_set_from_double(split);
+    for (auto const& val : tmp) {
+      Rprintf("Selected Factor Levels: %d\n", val);
+    }
+  }
+
+  return (R_NilValue);
+}
--- test.R
+++ test.R
@@ -12,61 +12,40 @@ x = titanic
 for (i in 1:8) x = rbind(x, x)
 
 mod = iForest(x[,-1], 500, phi=128)
-p1 = predict(mod, x, nodes=TRUE)
-
-p2 = predict(mod, x, method="r")
-p3 = predict(mod, x)
-
-
-## cluster based on nodes
-library(cluster)
-
 p1 = predict(mod, x[,-1], nodes=TRUE)
 
-f <- function(x, y) 1 - sum(x == y)/length(x)
-s = proxy::dist(p1, method=f)
-clus = hclust(s)
-k = cutree(clus, k = 10)
-tmp = svd(as.matrix(s), nu = 2, nv = 2)
+
+pca = prcomp(p1, center = TRUE, scale. = TRUE)
+
+library(glmnet)
+
+s = sample(nrow(p1), nrow(p1)/2)
+fit = cv.glmnet(x=pca$x, y=titanic$Survived, family="binomial", alpha=1)
+fit2 = cv.glmnet(x=p1[s,], y=titanic$Survived[s], family="binomial", alpha=1)
+lr = glm(Survived~., titanic, family="binomial")
+
+library(gbm)
+
+
+
+p = predict(fit2, p1, s="lambda.1se")
+
+library(mjollnir)
+
+ks.table(-p[s], titanic$Survived[s])
+ks.table(-p[-s], titanic$Survived[-s])
+ks.table(-predict(lr), titanic$Survived)
 
 library(igraph)
-g = graph.adjacency(1-as.matrix(s), weighted = TRUE, diag = FALSE, mode = "lower")
-
-library(rgexf)
-sink("test.gexf")
-rgexf::igraph.to.gexf(g)
-sink("")
-
-nodes_df <- data.frame(ID = c(1:vcount(g)), NAME = V(g)$name)
-# Create a dataframe edges: 1st column - source node ID, 2nd column -target node ID
-edges_df <- as.data.frame(get.edges(g, c(1:ecount(g))))
-
-write.gexf(nodes_df, edges_df, output = "test.gexf", nodesAtt = titanic, edgesWeight = E(g)$weight)
 
 
+t1 = mod$forest[[1]]
+f = which(t1[,1] == 1)
 
+al = cbind(rep(f, 2), as.character(t1[f,3:4]))
 
+g = graph_from_edgelist(al, directed = TRUE)
 
+plot(g, layout=layout_as_tree(g), vertex.size=4, edge.arrow.mode="-")
 
-p = predict(mod, d, nodes=TRUE)
-f <- function(x, y) 1-sum(x == y)/length(x)
-s = proxy::dist(p, method=f)
-clus = hclust(s)
-k = cutree(clus, k = 10)
-
-
-library(tsne)
-plt = tsne(s, k = 2)
-
-
-mod = iForest(x[,-1], 100, phi = 32)
-p = predict(mod, x[,-1], nodes=TRUE)
-## turn each tree into a one hot matrix
-
-sparse = matrix(0, nrow(p), ncol(p)*isofor:::max_nodes(ceiling(log2(32))))
-
-
-
-
-
-
+plot(g)
